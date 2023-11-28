52e1cb9a64169edeaa87528eeed9679b8892b604
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-11-28 14:58:56 -0500

    Matrices random

--- suma_matriz.c
+++ suma_matriz.c
@@ -9,5 +9,32 @@ Imprima la matriz A, B y el el resultado de su suma*/
 int main()
 {
     //Dimension de la matriz
+    int filas;
+    int columnas;
+    int i;
+    int j;
+    printf("ingrese el numero de filas de las matrices -> ");
+    scanf("%d", &filas);
+    printf("ingrese el numero de columnas de las matrices -> ");
+    scanf("%d", &columnas);
+
+    //Definir matrices
+    int A[filas][columnas];
+    int B[filas][columnas];
+    
+    //Matrices random
+    for ( i = 0; i < filas; i++)
+    {
+        for ( j = 0; j < columnas; j++)
+        {
+            A[i][j]=rand()%100;
+            B[i][j]=rand()%100;
+        }
+    }
+
+    
+    
+    
+    
     return 0;
 }
