83149a92629731a5fc870c1b5c2f6d00c848ccdb
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2023-11-28 14:39:41 -0500

    Creacion del programa

---
+++ suma_matriz.c
@@ -0,0 +1,13 @@
+/*Suma de matrices
+Usuario ingresa dimension de la matriz
+Usar numeroa aleatorios para cada matriz entre 0 y 100
+Imprima la matriz A, B y el el resultado de su suma*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    //Dimension de la matriz
+    return 0;
+}
